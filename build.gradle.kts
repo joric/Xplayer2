62e539383a537cffb64a1b4d88f8d9a447b9db43
joric <joric@users.noreply.github.com>
2026-04-15 03:40:43 +0500

    fixed illegal unicode escape sequence

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+// fix illegal unicode escape sequence
+subprojects {
+  tasks.withType<JavaCompile>().configureEach {
+    doFirst {
+      fileTree(rootDir).matching { include("**/build/generated/aidl_source_output_dir/**/*.java") }
+        .forEach {
+          val s = it.readText(); val r = s.replace('\\','/')
+          if (r != s) it.writeText(r)
+        }
+    }
+  }
+}
